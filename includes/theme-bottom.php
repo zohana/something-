d58cff95dce8fbce65677ec793c1e5a8e0ad7950
gite0002 <zohana28@yahoo.com>
2012-03-23 12:15:24 -0400

    bottom

---
+++ includes/theme-bottom.php
@@ -0,0 +1,5 @@
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDbQYTiRrft8Pi5JmZnvMj0s6THIrnfOxA&sensor=false"></script>
+	<script src="js/museums.js"></script>
+</body>
+</html>
